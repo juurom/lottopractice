caa8446349fa1180fcea24a8fc8877ff1cf2386c
juyeon <01bjy@kakao.com>
2022-12-28 14:16:08 +0900

    version 1.0.0

--- server/index.js
+++ server/index.js
@@ -3,17 +3,13 @@ const app = express();
 const cors = require('cors')
 const axios = require("axios");
 const cheerio = require("cheerio");
-const bodyParser = require('body-parser');
-const _ = require('lodash');
+require("dotenv").config();
+const MONGO_URI= process.env.MONGO_URI;
 
 app.use(cors({ credentials: true, origin: "http://localhost:5000" }));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
 
-const MONGO_URI='mongodb+srv://juyeon:sogang20190047@cluster0.anefahj.mongodb.net/?retryWrites=true&w=majority'
 const PORT = 5000;
-
-
 
 app.listen(PORT, () => console.log(`${PORT}포트입니다.`));
 
@@ -25,7 +21,6 @@ mongoose
     console.log(err);
   });
 
-// 스키마 정의
 const lotto = mongoose.Schema({
     index: Number,
     date: String,
@@ -37,96 +32,75 @@ const lotto = mongoose.Schema({
 
 const Lotto = mongoose.model('Schema', lotto);
 
+const fillDB=async()=>{
+  const html = await axios.get("https://signalfire85.tistory.com/28");
+  const $ = cheerio.load(html.data);
+  const $bodyList = $("tbody").children("tr");  
 
-async function getHtml(){
-  try {
-    return await axios.get("https://signalfire85.tistory.com/28");
-  } catch (error) {
-    console.error(error);
-  }
-};
+  $bodyList.each((i, elm)=>{
+    if (i<=2) { return; } //첫두줄은 내용 없음
+    let elements=['',]; //index 0 헷갈리니까 빈데이터 삽입
+    for(let i=1; i<=11; i++){
+      elements[i]=$(this).find(`td:nth-of-type(${i})`).text();
+    }
 
-// 이미 데이터를 저장했으면 더이상 받아오지 않음
-async function readDB(){
-  let promise = new Promise((resolve, reject)=>{
+    const newLotto = new Lotto({
+      index: Number(elements[1]),
+      date: elements[2],
+      firstWinner: Number(elements[3]),
+      firstPrize: elements[4],
+      winNum: [
+        Number(elements[5]),
+        Number(elements[6]),
+        Number(elements[7]),
+        Number(elements[8]),
+        Number(elements[9]),
+        Number(elements[10]),
+        Number(elements[11]),
+      ]
+    });
+
+    newLotto.save((error, data)=>{
+      if (error) console.log(error);
+      //else console.log(data);
+    });
+  });
+}
+
+const readDB = async()=>{
+  let promise = new Promise((res, rej)=>{
     Lotto.find((error, data)=>{
-      console.log(data.length==0)
       if (data.length==0) {
-        resolve(true);
+        res(true);
       }
-      else resolve(false);
+      else res(false);
    })
   })
   let empty = await promise;
   if (empty){
-    getHtml()
-    .then(html=>{
-  
-      const $ = cheerio.load(html.data);
-      const $bodyList = $("tbody").children("tr");  
-  
-      $bodyList.each(function(i, elm) {
-        if (i>2) { //첫두줄은 내용 없음
-          elements=['',]; //index 0 헷갈리니까 빈데이터 삽입
-            for(let i=1; i<=11; i++){
-              elements[i]=$(this).find(`td:nth-of-type(${i})`).text();
-            }
-  
-            const newLotto = new Lotto({
-            index: Number(elements[1]),
-            date: elements[2],
-            firstWinner: Number(elements[3]),
-            firstPrize: elements[4],
-            winNum: [
-              Number(elements[5]),
-              Number(elements[6]),
-              Number(elements[7]),
-              Number(elements[8]),
-              Number(elements[9]),
-              Number(elements[10]),
-              Number(elements[11]),
-          ]
-          });
-          newLotto.save((error, data)=>{
-            if (error){
-              console.log(error);
-            }else{
-              //console.log(data)
-            }
-          });
-        }
-    })
-    })
-  
+    fillDB();
   }
 }
 
 readDB();
-
-  
 
 app.get('/getAll',(req,res)=>{
   Lotto.find().sort({'index':1}).exec((error, lotto)=>{
     if (error){
       console.log(error);
     }else{
-      console.log(lotto);
+      //console.log(lotto);
       res.json(lotto);
     }
   });
 })
 
-
-let win=[];
-
 app.post('/numCheck',(req,res)=>{
-  win = [0, {index:0, date: 0, firstWinner: 0, firstPrize: 0, winNum:[]}];
+  console.log(req.body);
+  let win = [0, {index:0, date: 0, firstWinner: 0, firstPrize: 0, winNum:[]}];
   const inputs=(req.body.inputs);
-  inputs.sort();
   let winCount=0;
-  let bonus=false;
-
-  
+  let bonus=false;  
   Lotto.find({'winNum': {'$in': [...inputs]} },(error,lotto)=>{
     console.log('-------read found----------');
     if (error){
@@ -166,8 +140,6 @@ app.post('/numCheck',(req,res)=>{
       return res.send(win); 
     }
   })
-  
-
 })
 
 
@@ -177,11 +149,8 @@ app.post('/numCheck',(req,res)=>{
     if (error){
       console.log(error);
     }
-  })*/
-
-  //mongodb
-  //https://javafa.gitbooks.io/nodejs_server_basic/content/chapter12.html
-
+  })
+  */
   
 
 
